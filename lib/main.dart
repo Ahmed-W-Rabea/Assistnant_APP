fc933e9b705c2e96670713b823fd94f598c47891
Ahmed-W-Rabea <ahmedwael11425@gmail.com>
2024-09-25 23:56:54 +0300

    'add assets'

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:assistant_app/croe/constants/pallete.dart';
+import 'package:assistant_app/presentation/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,13 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      debugShowCheckedModeBanner: false,
+      title: 'AWR Helper',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor
       ),
-     home: null,
+     home: HomePage(),
     );
   }
 }
